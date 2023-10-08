838f9004de5155e558bfe03e2e569d6cfb2b0d9e
Thalis Freitas <ofthalis@gmail.com>
2023-10-07 22:41:01 -0300

    add generic error handling

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,3 +1,6 @@
 class Api::V1::ApiController < ActionController::API
-
+  rescue_from StandardError do |_exception|
+    render json: { error: 'Internal Server Error' },
+           status: :internal_server_error
+  end
 end
--- spec/requests/api/v1/loads_spec.rb
+++ spec/requests/api/v1/loads_spec.rb
@@ -1,23 +1,36 @@
 require 'rails_helper'
 
-describe 'Loads API', type: :request do
+describe Api::V1::LoadsController, type: :request do
   context 'GET /api/v1/loads' do
-
     before do
       @loads = create_list(:load, 5)
-      get '/api/v1/loads'
-    end
-    
-    it { expect(response).to have_http_status(:success) }
-
-    it 'returns the expected number of loads' do
-      expect(json[:loads].count).to eql(5)
     end
 
-    it 'success' do
-      json[:loads].each do |load|
-        expect(load.keys).to include(*%i[id code delivery_date])
-        expect(load.keys).not_to include(*%i[created_at updated_at])
+    context 'successful requests' do
+      it 'returns the expected number of loads' do
+        get '/api/v1/loads'
+
+        expect(response).to have_http_status(:success)
+        expect(json[:loads].count).to eql(5)
+      end
+
+      it 'returns loads with expected attributes' do
+        get '/api/v1/loads'
+
+        json[:loads].each do |load|
+          expect(load.keys).to include(*%i[id code delivery_date])
+          expect(load.keys).not_to include(*%i[created_at updated_at])
+        end
+      end
+    end
+
+    context 'error handling' do
+      it 'handles internal server error' do
+        allow(Load).to receive(:page).and_raise(ActiveRecord::QueryCanceled)
+        get '/api/v1/loads'
+
+        expect(response).to have_http_status(:internal_server_error)
+        expect(json).to include(error: 'Internal Server Error')
       end
     end
   end
