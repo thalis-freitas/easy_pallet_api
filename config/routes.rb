e013e9a9d2ba841df29adc7ce66705d55be5c04f
Thalis Freitas <ofthalis@gmail.com>
2023-10-08 18:15:27 -0300

    fix "except" keyword in resource route definitions for show action in products and loads

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :products, expect: [:show]
+      resources :products, except: :show
 
-      resources :loads, expect: [:show] do
+      resources :loads, except: :show do
         resources :orders, only: [:index, :create]
       end
 
