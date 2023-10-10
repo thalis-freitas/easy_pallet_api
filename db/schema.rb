5674e78f921eeae8e7b5e73d6adb48b735fd6a5e
Thalis Freitas <ofthalis@gmail.com>
2023-10-10 18:38:32 -0300

    create User model with unique login index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_08_232932) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_10_211729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_08_232932) do
     t.datetime "updated_at", null: false
     t.index ["order_id"], name: "index_sorted_order_products_on_order_id"
     t.index ["product_id"], name: "index_sorted_order_products_on_product_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "login"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["login"], name: "index_users_on_login", unique: true
   end
 
   add_foreign_key "order_products", "orders"
